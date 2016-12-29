f5b2fd1caa534e7a5fbec7e2e8316cbaea31465b
BJTU-HT <bjtu-ht@163.com>
2016-12-29 11:07:50 +0800

    Initial Commit

---
+++ TeamTalkSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeamTalkSwift
+//
+//  Created by tom555cat on 2016/12/29.
+//  Copyright © 2016年 Hello World Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
